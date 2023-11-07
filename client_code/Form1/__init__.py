e7db676065d60c7ed012ab83e69a97c95f0160c9
gabiprodan@gmail.com <gabiprodan@gmail.com>
2023-11-07 22:40:47 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,6 +1,11 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
 
+mlChestx_models = ['PSPNet', ]
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
