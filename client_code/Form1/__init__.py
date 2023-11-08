245315893b407b1bc5a54247028128a02d589124
gabiprodan@gmail.com <gabiprodan@gmail.com>
2023-11-08 17:39:37 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -5,10 +5,19 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.users
 
-mlChestx_models = ['PSPNet', ]
+mlChestx_models = ['PSPNet', 'cx']
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.ddModel.items = mlChestx_models
 
     # Any code you write here will run before the form opens.
+
+  def ddModel_change(self, **event_args):
+    """This method is called when an item is selected"""
+    self.lblModelIndex.text = str(self.ddModel.selected_value)
+
+  def file_loader_1_change(self, file, **event_args):
+    """This method is called when a new file is loaded into this FileLoader"""
+    self.image_1.source = file
